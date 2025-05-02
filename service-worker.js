48dd486fdde3bb5a958be7871226d05b55fb09fd
Giovani <giovanifreitasdc@gmail.com>
2025-05-02 17:39:04 -0300

    Função para IOS para criar um app no iphone

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', () => {
+    console.log('Service Worker instalado');
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(fetch(event.request));
+  });
+  
